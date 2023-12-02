dec5180666dea2ddf30129b7c0d1b3fb0b5cb3b7
JúDCraide <juliadcraide31@gmail.com>
2023-12-02 17:03:37 -0300

    Detect obstacle collision

---
+++ Assets/Scripts/DamagePlayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Player")) {
+            Debug.Log("Player Colided");
+        }
+    }
+}
