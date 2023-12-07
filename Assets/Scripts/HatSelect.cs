9349c9ff4428fa9eda1c4628c5ab4a3c7a3f6f2a
Júlia D. Craide <juliadcraide@hotmail.com>
2023-12-07 17:32:29 -0300

    Hat select working between saves

---
+++ Assets/Scripts/HatSelect.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HatSelect : MonoBehaviour {
+    [SerializeField] public Hat hat;
+
+    [SerializeField] private TMPro.TextMeshProUGUI hatText;
+    [SerializeField] private Image hatImage;
+
+    public void Start() {
+        hatImage.GetComponent<Image>().sprite = hat.sprite;
+        Hat savedHat = Hats.GetHat(hat.id);
+        if (savedHat == null) {
+            Hats.SetHat(hat.id, hat);
+        }
+        else {
+            hat.bought = savedHat.bought;
+            hat.selected = savedHat.selected;
+        }
+        hatText.text = hat.SetText();
+    }
+
+    public void Update() {
+        hatText.text = hat.SetText();
+    }
+
+    public void OnClick() {
+        if (!hat.bought) {
+            hat.BuyHat();
+        }
+        else if (!hat.selected) {
+            hat.SelectHat();
+        }
+    }
+}
--- Assets/Scripts/Hats.cs
+++ Assets/Scripts/Hats.cs
@@ -1,34 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
 public static class Hats {
-    [SerializeField] private static List<Hat> hats = new List<Hat> { null, null, null, null, null, null, null, null, null, null };
+    [SerializeField] public static List<Hat> hats; //= new List<Hat> { null, null, null, null, null, null, null, null, null, null };
     [SerializeField] public static Hat selectedHat = null;
 
+    static Hats() {
+        Debug.Log("construtor");
+        Hat hat = new();
+        Hats.hats = new List<Hat> { null, null, null, null, null, null, null, null, null, null };
+    }
+
     public static Hat GetHat(int id) {
-        //if (hats[id] == null) {
-        //    Debug.Log("null");
-        //}
-        //else {
-        //    Debug.Log(hats[id].selected.ToString());
-        //}
-        return hats[id];
+        if (Hats.hats[id] == null) {
+            Debug.Log("null");
+        }
+        else {
+            Debug.Log(Hats.hats[id].ToString());
+        }
+        return Hats.hats[id];
     }
 
     public static void SetHat(int id, Hat h) {
-        hats[id] = h;
+        //foreach (Hat hat in hats) {
+        //    if (hat) {
+        //        Debug.Log(hat);
+        //    }
+        //}
+        Hats.hats[id] = h;
     }
 
     public static void selectHat(Hat h) {
-        if (selectedHat != null) {
-            selectedHat.UnselectHat();
+        if (Hats.selectedHat != null) {
+            Hats.selectedHat.UnselectHat();
         }
-        selectedHat = h;
+        Hats.selectedHat = h;
     }
 
     public static void unselectHat() {
-        selectedHat.UnselectHat();
-        selectedHat = null;
+        Hats.selectedHat.UnselectHat();
+        Hats.selectedHat = null;
     }
 }
