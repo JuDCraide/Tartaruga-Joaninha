ea0bd55457fa47559e5b9cffd1aab5974a823a61
Júlia D. Craide <juliadcraide@hotmail.com>
2023-12-11 04:04:53 -0300

    Level 2 and Fish Enemy

---
+++ Assets/Scripts/FishEnemy.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FishEnemy : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    private Animator enemyAnimator;
+
+    [Header("Ground Movement")]
+    [SerializeField] float moveSpeed = 1.0f;
+    [SerializeField] float moveDirection = -1;
+    [SerializeField] bool turnedRight = false;
+
+    [Header("Wall Detect")]
+    public LayerMask wallLayer;
+    [SerializeField] private Transform leftPosition;
+    [SerializeField] private Transform rightPosition;
+    [SerializeField] private Transform topPosition;
+    [SerializeField] private Transform bottomPosition;
+    [SerializeField] float wallDetectRadius = 0.2f;
+    [SerializeField] private bool verticalMovement;
+
+    void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+        enemyAnimator = GetComponent<Animator>();
+        if (verticalMovement) {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+
+    void FlipHorizontal() {
+        bool leftCollided = Physics2D.OverlapCircle(leftPosition.position, wallDetectRadius, wallLayer);
+        bool rightCollided = Physics2D.OverlapCircle(rightPosition.position, wallDetectRadius, wallLayer);
+
+        if ((turnedRight && rightCollided) || (!turnedRight && leftCollided)) {
+            moveDirection *= -1;
+            turnedRight = !turnedRight;
+            GetComponent<SpriteRenderer>().flipX = turnedRight;
+        }
+    }
+
+    void FlipVertical() {
+        bool topCollided = Physics2D.OverlapCircle(topPosition.position, wallDetectRadius, wallLayer);
+        bool bottomCollided = Physics2D.OverlapCircle(bottomPosition.position, wallDetectRadius, wallLayer);
+
+        if ( bottomCollided ||  topCollided) {
+            moveDirection *= -1;
+        }
+    }
+
+    void Update() {
+        if (verticalMovement) {
+            FlipVertical();
+        }
+        else {
+            FlipHorizontal();
+        }
+    }
+
+
+    void FixedUpdate() {
+        if (verticalMovement) {
+            rb.velocity = new Vector2(rb.velocity.x, moveDirection * moveSpeed);
+        }
+        else {
+            rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+        }
+    }
+}
